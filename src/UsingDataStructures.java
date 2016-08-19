90b9c643938a3fcf5cc93ecf7b6b8acc5efa49c1
duhlarik <DeeAnn.Uhlarik@gmail.com>
2016-08-18 22:51:27 -0400

    first push of all files

---
+++ src/UsingDataStructures.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class UsingDataStructures {
+
+	public static void main(String[] args) {
+
+		Stack<Integer> s = new Stack<Integer>();
+		
+		s.push(5);
+		s.push(15);
+		s.push(1);
+		
+		System.out.println(s.pop());
+	}
+
+}
